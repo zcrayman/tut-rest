50e28a402a2714c9fe36372aa0bacf7c2d0bd6bf
Russ Miles <russ@russmiles.com>
2013-08-08 17:20:23 +0100

    Added a controllers test

--- step1/src/test/java/com/yummynoodlebar/rest/controller/OrdersControllerTest.java
+++ step1/src/test/java/com/yummynoodlebar/rest/controller/OrdersControllerTest.java
@@ -1,6 +1,7 @@
 package com.yummynoodlebar.rest.controller;
 
 import com.yummynoodlebar.core.services.OrderService;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
@@ -11,6 +12,13 @@ public class OrdersControllerTest {
 
   OrderService orderService;
   OrdersController controller;
+
+  @Before
+  public void setup() {
+    controller = new OrdersController();
+    orderService = mock(PriceHistoryServiceImpl.class);
+    controller.setSecurityPriceService(mockSecurityPriceService);
+  }
 
   @Test
   void getsAllOrders() {
