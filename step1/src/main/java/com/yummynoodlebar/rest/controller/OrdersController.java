c845832f81cf7b0e4c0143a566162c7e82247e76
Russ Miles <russ@russmiles.com>
2013-08-09 14:43:56 +0100

    Progressing the code

--- step1/src/main/java/com/yummynoodlebar/rest/controller/OrdersController.java
+++ step1/src/main/java/com/yummynoodlebar/rest/controller/OrdersController.java
@@ -4,19 +4,21 @@ import com.yummynoodlebar.core.events.orders.OrderDetails;
 import com.yummynoodlebar.core.events.orders.RequestAllOrdersEvent;
 import com.yummynoodlebar.core.services.OrderService;
 import com.yummynoodlebar.rest.domain.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 @Controller
 @RequestMapping("/aggregators/orders")
 class OrdersController {
+
+  static Logger LOG = LoggerFactory.getLogger(OrdersController.class);
 
   @Autowired
   private OrderService orderService;
@@ -30,8 +32,10 @@ class OrdersController {
   @ResponseBody public List<Order> getAllOrders() {
     List<Order> orders = new ArrayList<Order>();
     for (OrderDetails detail : orderService.requestAllOrders(new RequestAllOrdersEvent()).getOrdersDetails()) {
+
       orders.add(new Order());
     }
+    LOG.warn("converting ... RETURNIN NOW");
     return orders;
   }
 
--- step1/src/test/java/com/yummynoodlebar/rest/controller/OrdersControllerIntegrationTest.java
+++ step1/src/test/java/com/yummynoodlebar/rest/controller/OrdersControllerIntegrationTest.java
@@ -1,39 +1,25 @@
 package com.yummynoodlebar.rest.controller;
 
-import com.yummynoodlebar.core.CoreConfig;
+import com.yummynoodlebar.core.domain.Order;
 import com.yummynoodlebar.core.events.orders.AllOrdersEvent;
-import com.yummynoodlebar.core.events.orders.OrderDetails;
 import com.yummynoodlebar.core.events.orders.RequestAllOrdersEvent;
 import com.yummynoodlebar.core.services.OrderService;
-import com.yummynoodlebar.rest.MVCConfig;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-import org.springframework.web.context.WebApplicationContext;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.mockito.Mockito.*;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(classes = {MVCConfig.class, CoreConfig.class})
 public class OrdersControllerIntegrationTest {
-  @Autowired
-  private WebApplicationContext wac;
 
   private MockMvc mockMvc;
 
@@ -46,12 +32,11 @@ public class OrdersControllerIntegrationTest {
 
   @Before
   public void setup() {
-
     MockitoAnnotations.initMocks(this);
 
-    when(orderService.requestAllOrders(any(RequestAllOrdersEvent.class))).thenReturn(new AllOrdersEvent(Collections.singletonMap(new OrderDetails())));
+    this.mockMvc = standaloneSetup(controller).build();
 
-    this.mockMvc = webAppContextSetup(this.wac).build();
+    when(orderService.requestAllOrders(any(RequestAllOrdersEvent.class))).thenReturn(allOrders());
   }
 
   @Test
@@ -62,5 +47,14 @@ public class OrdersControllerIntegrationTest {
         .accept(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk())
         .andExpect(jsonPath("$[0].items['yumm1']").value(12));
+  }
+
+  static AllOrdersEvent allOrders() {
+    //TODO, exposing the core domain in the tests is nasty. AllOrdersEvent needs to remove coupling directly.
+    Map<UUID, Order> orders = new HashMap<UUID, Order>();
+
+    orders.put(UUID.randomUUID(), new Order(new Date()));
+
+    return new AllOrdersEvent(orders);
   }
 }
