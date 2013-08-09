334a7a0e1adff256be3b4a41c0e8ffff06e8de72
Russ Miles <russ@russmiles.com>
2013-08-09 14:04:43 +0100

    More updates to Order in the REST domain

--- step1/src/main/java/com/yummynoodlebar/rest/controller/OrdersController.java
+++ step1/src/main/java/com/yummynoodlebar/rest/controller/OrdersController.java
@@ -1,5 +1,7 @@
 package com.yummynoodlebar.rest.controller;
 
+import com.yummynoodlebar.core.events.orders.OrderDetails;
+import com.yummynoodlebar.core.events.orders.RequestAllOrdersEvent;
 import com.yummynoodlebar.core.services.OrderService;
 import com.yummynoodlebar.rest.domain.Order;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
@@ -25,7 +28,11 @@ class OrdersController {
   @RequestMapping(method = RequestMethod.GET)
   @ResponseStatus( HttpStatus.OK )
   @ResponseBody public List<Order> getAllOrders() {
-    return Arrays.asList(new Order());
+    List<Order> orders = new ArrayList<Order>();
+    for (OrderDetails detail : orderService.requestAllOrders(new RequestAllOrdersEvent()).getOrdersDetails()) {
+      orders.add(new Order());
+    }
+    return orders;
   }
 
 }
--- step1/src/test/java/com/yummynoodlebar/rest/controller/OrdersControllerIntegrationTest.java
+++ step1/src/test/java/com/yummynoodlebar/rest/controller/OrdersControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package com.yummynoodlebar.rest.controller;
 
 import com.yummynoodlebar.core.CoreConfig;
 import com.yummynoodlebar.core.events.orders.AllOrdersEvent;
+import com.yummynoodlebar.core.events.orders.OrderDetails;
 import com.yummynoodlebar.core.events.orders.RequestAllOrdersEvent;
 import com.yummynoodlebar.core.services.OrderService;
 import com.yummynoodlebar.rest.MVCConfig;
@@ -10,6 +11,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
@@ -19,30 +21,37 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {MVCConfig.class})
-//@WebAppConfiguration
+@WebAppConfiguration
+@ContextConfiguration(classes = {MVCConfig.class, CoreConfig.class})
 public class OrdersControllerIntegrationTest {
+  @Autowired
+  private WebApplicationContext wac;
 
+  private MockMvc mockMvc;
+
+  @InjectMocks
+  private OrdersController controller;
 
   @Mock
   private OrderService orderService;
 
-  @InjectMocks
-  OrdersController controller;
-
-  private MockMvc mockMvc;
 
   @Before
   public void setup() {
 
-    this.mockMvc = standaloneSetup(controller).build();
+    MockitoAnnotations.initMocks(this);
 
-    when(orderService.requestAllOrders(any(RequestAllOrdersEvent.class))).thenReturn(new AllOrdersEvent(null));
+    when(orderService.requestAllOrders(any(RequestAllOrdersEvent.class))).thenReturn(new AllOrdersEvent(Collections.singletonMap(new OrderDetails())));
+
+    this.mockMvc = webAppContextSetup(this.wac).build();
   }
 
   @Test
