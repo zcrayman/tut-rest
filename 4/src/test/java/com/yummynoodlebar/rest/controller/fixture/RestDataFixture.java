7195826d8016ff8cdea30961cc8a2da2af34caa0
Russ Miles <russ@russmiles.com>
2013-08-13 21:00:46 +0100

    Clean code

--- 4/src/test/java/com/yummynoodlebar/rest/controller/fixture/RestDataFixture.java
+++ 4/src/test/java/com/yummynoodlebar/rest/controller/fixture/RestDataFixture.java
@@ -8,13 +8,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.UUID;
 
-//TODOCUMENT.  Use of test data fixture classes is considered good practice.
-/*
- The majority of tests aren't testing data edge cases, they are testing logical flows and
- what happens to a generic set of data.  For these, use a small, standardised set of fixtures.
-
- For anything more esoteric, create a new fixture in the test class.
- */
 public class RestDataFixture {
   public static final String YUMMY_ITEM = "yummy1";
 
